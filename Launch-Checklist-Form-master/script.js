8c79dd171e56b81eaebc5eb041253941444051e5
Logan Taylor <Stellarate.Official@gmail.com>
2019-10-23 15:52:35 -0500

    Updated index.html and script.js files to add more progress

--- Launch-Checklist-Form-master/script.js
+++ Launch-Checklist-Form-master/script.js
@@ -1,13 +1,24 @@
 // Write your JavaScript code here!
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+// This block of code shows how to format the HTML once you fetch some planetary JSON!
+window.addEventListener("load", function() {
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+      response.json().then( function(json) {
+         console.log(json);
+         const div = document.getElementById("missionTarget");
+         // Add HTML that includes the JSON data
+        let data = json;
+         div.innerHTML = `
+         <h2>Mission Destination</h2><ol>
+         <li>Name:${data.name}</li> 
+         <li>Diameter: ${data.diameter}</li>
+         <li>Star: ${data.star}</li>
+         <li>Distance from Earth: ${data.distance}</li>
+         <li>Number of Moons: ${data.moons}</li>
+         <img src=${"https://mars.nasa.gov/system/resources/detail_files/7808_global-color-views-mars-PIA00407-full2.jpg"}
+         </ol>
+         `
+    
+    })
+    })
+    });
